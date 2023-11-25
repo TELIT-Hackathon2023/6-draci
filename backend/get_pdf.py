20619e7d2a6b2a5ed682081278fbd065e88b966d
Samuel Gallik <samuel.gallik@student.tuke.sk>
2023-11-25 22:12:57 +0100

    uplad site working with backend

--- backend/get_pdf.py
+++ backend/get_pdf.py
@@ -5,11 +5,16 @@ app = Flask(__name__)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'pdfFile' in files:
-        pdf = request.files['pdfFile']
-        filename = secure_filename(pdf.filename)
-        custom_filename = 'check.pdf'
-        pdf.save('./files/'+custom_filename)
+
+        if 'pdfFile' in request.files:
+            pdf = request.files['pdfFile']
+            filename = secure_filename(pdf.filename)
+            custom_filename = 'check.pdf'
+            pdf.save('./files/' + custom_filename)
+            return "File uploaded successfully", 200
+        else:
+            return "No file received", 400
 
 if __name__ == '__main__':
     app.run(debug=True)
+
