d54363eedf89060c2249be4f4b01d69543c1f976
dodo <dodobudzak@icloud.com>
2023-11-26 05:09:17 +0100

    testing model

--- backend/main.py
+++ backend/main.py
@@ -1,32 +1,41 @@
-from langchain.llms import  OpenAI
-from   langchain.chains.question_answering import load_qa_chain
-from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
-import  pinecone
-loader = PyPDFLoader("test.pdf")
-key = ""
-data=loader.load()
+from langchain.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import  OpenAI
+from langchain.chains import RetrievalQA
+from langchain.evaluation import load_evaluator
+from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader, TextLoader
+from sklearn.metrics.pairwise import cosine_similarity
+import os
+import nltk
 
-llm = OpenAI(temperature=0.9,openai_api_key=key)
-chain = load_qa_chain(llm,chain_type="stuff")
-text_spliter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=0)
-texts = text_spliter.split_documents(data)
+openai_api_key = ""
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+evaluator = load_evaluator("pairwise_embedding_distance",embeddings=embeddings)
+loader = TextLoader("dataset_technical.txt")
+loader2 = PyPDFLoader("test.pdf")
+query = "Which programing languages are  used for software"
+query2 = "There any Assistance to disabled people"
 
-pinecone.init(
-    api_key="",
-    environment="gcp-starter"
+def compareEmbeddings(string1,string2):
+    return evaluator.evaluate_string_pairs(
+        prediction=string1, prediction_b=string2
+    )
+def embedCriteria(loader,query):
 
-)
-query = "which attachment is Score Summary Matrix "
-emb = OpenAIEmbeddings(openai_api_key=key)
-docsearch = Pinecone.from_texts([t.page_content for t in texts],emb,index_name="copilot1")
-docs = docsearch.similarity_search(query)
-##print(texts[1])
-print(chain.run(input_documents=docs,question=query))
+    data=loader.load()
 
-##print(docs[0])
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(data)
+    docsearch = FAISS.from_documents(texts, embeddings)
 
+
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever())
+    result= qa.run(query)
+    return result
+
+string1 = embedCriteria(loader,query)
+string2 = embedCriteria(loader2,query2)
+print(compareEmbeddings(string1, string2))
 
